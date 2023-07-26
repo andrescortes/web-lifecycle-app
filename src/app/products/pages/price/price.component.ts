1de9e1ae05b7fcc95a627c68b6511eac1ee4c1d6
Miguel Cortes <miguel.cortes@ias.com.co>
2023-07-25 21:05:46 -0500

    testing hooks

--- src/app/products/pages/price/price.component.ts
+++ src/app/products/pages/price/price.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrls: ['./price.component.css']
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input()
+  public price: number = 0;
+  public interval$?: Subscription;
+
+
+  ngOnInit(): void {
+    console.log('PriceComponent: ngOnInit');
+    this.interval$ = interval(1000).subscribe(value => console.log(`Tick: ${value}`));
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({ changes });
+    console.log('PriceComponent: ngOnChanges');
+
+  }
+  ngOnDestroy(): void {
+    console.log('PriceComponent: ngOnDestroy');
+    this.interval$?.unsubscribe();
+  }
 
 }
